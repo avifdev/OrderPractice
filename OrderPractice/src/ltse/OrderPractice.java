23f28d4687293a8333c6ea61315137b0ee297fea
Avi Feigenbaum <afeigenbaum@outlook.com>
2018-01-25 14:56:39 -0500

    Adding main()

---
+++ OrderPractice/src/ltse/OrderPractice.java
@@ -0,0 +1,13 @@
+package ltse;
+
+public class OrderPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		if (args.length != 3) {
+			System.out.println("Usage: ProcessOrders symbolsFile firmsFile orderFile");
+			return;
+		}
+	}
+
+}
